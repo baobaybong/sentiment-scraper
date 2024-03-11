4a2ba45ea9d23df00d64c712a21c6270e9e8f356
baobaybong <baorng@gmail.com>
2024-03-12 01:37:58 +0800

    init

--- app.py
+++ app.py
@@ -12,3 +12,6 @@ def predict():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=7860)
