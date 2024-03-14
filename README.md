cd02f51ddc6204f0b65b75a38695b9768892b6d0
baobaybong <baorng@gmail.com>
2024-03-14 18:50:04 +0800

    added scraping module and README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ---
-title: Sentiment Crawler
+title: Sentiment Scraper
 emoji: 🏃
 colorFrom: pink
 colorTo: yellow
@@ -10,4 +10,18 @@ pinned: false
 license: mit
 ---
 
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+# Sentiment Scraper
+
+"End-to-end" sentiment analysis project (fancy name)
+
+- EDA on Twitter sentiment140 dataset: https://github.com/baobaybong/sentiment-scraper/blob/main/eda.ipynb
+- Modelling: simple TF-IDF + Naive Bayes model with 76% test accuracy
+- Deployment: simple website that can scrape the latest X (Twitter) tweets about a certain person/topic and analyze the sentiment of the tweets. Link: https://huggingface.co/spaces/baobaybong/sentiment-scraper
+
+![demo](static/images/demo.png)
+
+## Limitations
+- As you can see, the website currently does not have any front-end, very ugly. The back-end code is ugly as well.
+- The model can be improved, currently it is very simple and not very accurate. It is also a sentiment binary classification model, which doesn't account for the intensity or neutrality of sentiments.
+- Functionalities can be added. 
+- Due to Twitter's API regulation, only a maximum of 300 tweets can be scraped per account per day. The website will scrape less than 50 tweets per request to avoid hitting the limit. (I don't know how legal is scraping with multiple accounts T_T).
--- app.py
+++ app.py
@@ -1,17 +1,99 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, Response
 import joblib
+
+from twscrape import API
+import pandas as pd
+from pytz import timezone
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import io
+import sqlite3
+from dotenv import load_dotenv
+import os
+import asyncio
 
 app = Flask(__name__)
 model = joblib.load('models/model.joblib')
+api = API()
+matplotlib.use('agg')
+scraped_data = None
+
+async def setup():
+    load_dotenv()
+    X_CREDENTIAL = os.getenv('X_CREDENTIAL')
+    await api.pool.add_account(*X_CREDENTIAL.split(','))
+    await api.pool.login_all()
+
+def create_figure(df):
+    cnt = df.sentiment.value_counts()
+
+    fig, ax = plt.subplots(figsize=(9.2, 2))
+
+    colors = ['green', 'orange']
+    pd.DataFrame(cnt).T.plot(kind='barh', stacked=True, width=0.5, color=colors, ax=ax)
+
+    for i, rect in enumerate(ax.patches):
+        width = rect.get_width()
+        height = rect.get_height()
+        x = rect.get_x()
+        y = rect.get_y()
+        
+        ax.text(x + width / 2, y + height / 2, cnt.iloc[i], ha='center', va='center', color='white', fontsize=12)
+
+    ax.set_title(f"Latest X's sentiment for {request.args.get('keyword')}")
+    ax.legend(ncols=2)
+    ax.axis('off')
+
+    return fig
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['GET'])
 def predict():
     text = request.args.get('text')
     return model.predict([text])[0]
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+@app.route('/scrape', methods=['GET'])
+async def scrape():
+    global scraped_data
+    keyword = request.args.get('keyword')
+
+    # tweets_df = pd.DataFrame({'content': ['hi', 'lol', keyword], 'b': [2, 3, 4]})
+    # tweets_df['sentiment'] = model.predict(tweets_df.content)
+
+    tweets = []
+    
+    cols = ['username', 'date', 'url', 'content']
+
+    async for tweet in api.search(f"{keyword} lang:en", limit=30):
+        tweets.append((tweet.user.username, tweet.date, tweet.url, tweet.rawContent))
+
+    tweets_df = pd.DataFrame(tweets, columns=cols)
+    tweets_df['date'] = tweets_df['date'].apply(lambda x: x.astimezone(timezone('Asia/Singapore')).strftime("%d-%m-%Y %H:%M:%S UTC%Z"))
+    tweets_df['url'] = tweets_df['url'].apply(lambda x: f'<a href="{x}" target="_blank">{x}</a>')
+
+    tweets_df['sentiment'] = model.predict(tweets_df.content)
+
+    scraped_data = tweets_df.to_json(orient='records', index=False)
+    return tweets_df.to_html(escape=False)
+
+@app.route('/plot', methods=['GET'])
+def plot():
+    df = pd.read_json(io.StringIO(scraped_data))
+
+    fig = create_figure(df)
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+
+    return Response(output.getvalue(), mimetype='image/png')
+
+async def main():
+    await setup()
+    app.run(host='0.0.0.0', port=7860)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=7860)
+    asyncio.run(main())
